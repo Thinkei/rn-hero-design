fd455e8a1e77e2313aab12abe1d3df9157cb9735
Toan Nguyen <toan.nguyen@employmenthero.com>
2020-09-01 15:38:38 +0700

    Config metro to resolve symlink

--- playground/metro.config.js
+++ playground/metro.config.js
@@ -4,6 +4,13 @@
  *
  * @format
  */
+const path = require('path');
+
+const libPath = path.resolve(`${__dirname}/../lib`);
+
+const extraNodeModules = { lib: libPath };
+
+const watchFolders = [libPath];
 
 module.exports = {
   transformer: {
@@ -14,4 +21,13 @@ module.exports = {
       },
     }),
   },
+  resolver: {
+    extraNodeModules: new Proxy(extraNodeModules, {
+      get: (target, name) =>
+        name in target
+          ? target[name]
+          : path.join(process.cwd(), `node_modules/${name}`),
+    }),
+  },
+  watchFolders,
 };
